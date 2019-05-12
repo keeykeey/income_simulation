f4726d4af2e020e22b9e6f65065631d49c144357
Shin Fujito <shinfujito@ShinnoMacBook-Pro.local>
2019-05-12 14:43:57 +0900

    2019050121443

---
+++ Simulator_GUI.py
@@ -0,0 +1,351 @@
+import tkinter as tk 
+
+root = tk.Tk()
+root.title('Income_simulation')
+root.geometry('600x700')
+
+label0 =tk.Label(root,text="生涯年収を計算します",font=('',20))
+label0.place(x=0,y=0)
+#label0.pack()
+
+label1 = tk.Label(root,text='(1) 入都する時の年齢を入力してください（23歳以上）。')
+label1.place(x=5,y=35)
+editbox1 = tk.Entry(root,width=2,borderwidth=1)
+editbox1.place(x=15,y=55)
+
+label2_1 = tk.Label(root,text='(2) 以下の入力欄に、各年齢での昇給幅(0〜6)を入力して下さい')
+label2_1.place(x=5,y=75)
+label2_2 = tk.Label(root,text='クビレベル→1 / ヤバイ→2 / 調子が悪かった→3 / 普通→4 / まあまあ良い→5 / 大活躍→6')
+label2_2.place(x=15,y=95)
+
+label2_23 = tk.Label(root,text='23歳          号昇給')
+#label2_23.place(x=15,y=175)
+ebox23 = tk.Entry(width=2,borderwidth=1)
+ebox23.insert(tk.END,4)
+#ebox23.place(x=55,y=175)
+
+label2_24 = tk.Label(root,text='24歳          号昇給')
+label2_24.place(x=15,y=195)
+ebox24 = tk.Entry(width=2,borderwidth=1)
+ebox24.insert(tk.END,4)
+ebox24.place(x=55,y=195)
+
+label2_25 = tk.Label(root,text='25歳          号昇給')
+label2_25.place(x=15,y=215)
+ebox25 = tk.Entry(width=2,borderwidth=1)
+ebox25.insert(tk.END,4)
+ebox25.place(x=55,y=215)
+
+label2_26 = tk.Label(root,text='26歳          号昇給')
+label2_26.place(x=15,y=235)
+ebox26 = tk.Entry(width=2,borderwidth=1)
+ebox26.insert(tk.END,4)
+ebox26.place(x=55,y=235)
+
+label2_27 = tk.Label(root,text='27歳          号昇給')
+label2_27.place(x=15,y=255)
+ebox27 = tk.Entry(width=2,borderwidth=1)
+ebox27.insert(tk.END,4)
+ebox27.place(x=55,y=255)
+
+label2_28 = tk.Label(root,text='28歳          号昇給')
+label2_28.place(x=15,y=275)
+ebox28 = tk.Entry(width=2,borderwidth=1)
+ebox28.insert(tk.END,4)
+ebox28.place(x=55,y=275)
+
+label2_29 = tk.Label(root,text='29歳          号昇給')
+label2_29.place(x=15,y=295)
+ebox29 = tk.Entry(width=2,borderwidth=1)
+ebox29.insert(tk.END,4)
+ebox29.place(x=55,y=295)
+
+label2_30 = tk.Label(root,text='30歳          号昇給')
+label2_30.place(x=15,y=315)
+ebox30 = tk.Entry(width=2,borderwidth=1)
+ebox30.insert(tk.END,4)
+ebox30.place(x=55,y=315)
+
+label2_31 = tk.Label(root,text='31歳          号昇給')
+label2_31.place(x=155,y=135)
+ebox31 = tk.Entry(width=2,borderwidth=1)
+ebox31.insert(tk.END,4)
+ebox31.place(x=195,y=135)
+
+label2_32 = tk.Label(root,text='32歳          号昇給')
+label2_32.place(x=155,y=155)
+ebox32 = tk.Entry(width=2,borderwidth=1)
+ebox32.insert(tk.END,4)
+ebox32.place(x=195,y=155)
+
+label2_33 = tk.Label(root,text='33歳          号昇給')
+label2_33.place(x=155,y=175)
+ebox33 = tk.Entry(width=2,borderwidth=1)
+ebox33.insert(tk.END,4)
+ebox33.place(x=195,y=175)
+
+label2_34 = tk.Label(root,text='34歳          号昇給')
+label2_34.place(x=155,y=195)
+ebox34 = tk.Entry(width=2,borderwidth=1)
+ebox34.insert(tk.END,4)
+ebox34.place(x=195,y=195)
+
+label2_35 = tk.Label(root,text='35歳          号昇給')
+label2_35.place(x=155,y=215)
+ebox35 = tk.Entry(width=2,borderwidth=1)
+ebox35.insert(tk.END,4)
+ebox35.place(x=195,y=215)
+
+label2_36 = tk.Label(root,text='36歳          号昇給')
+label2_36.place(x=155,y=235)
+ebox36 = tk.Entry(width=2,borderwidth=1)
+ebox36.insert(tk.END,4)
+ebox36.place(x=195,y=235)
+
+label2_37 = tk.Label(root,text='37歳          号昇給')
+label2_37.place(x=155,y=255)
+ebox37 = tk.Entry(width=2,borderwidth=1)
+ebox37.insert(tk.END,4)
+ebox37.place(x=195,y=255)
+
+label2_38 = tk.Label(root,text='38歳          号昇給')
+label2_38.place(x=155,y=275)
+ebox38 = tk.Entry(width=2,borderwidth=1)
+ebox38.insert(tk.END,4)
+ebox38.place(x=195,y=275)
+
+label2_39 = tk.Label(root,text='39歳          号昇給')
+label2_39.place(x=155,y=295)
+ebox39 = tk.Entry(width=2,borderwidth=1)
+ebox39.insert(tk.END,4)
+ebox39.place(x=195,y=295)
+
+label2_40 = tk.Label(root,text='40歳          号昇給')
+label2_40.place(x=155,y=315)
+ebox40 = tk.Entry(width=2,borderwidth=1)
+ebox40.insert(tk.END,4)
+ebox40.place(x=195,y=315)
+
+label2_41 = tk.Label(root,text='41歳          号昇給')
+label2_41.place(x=295,y=135)
+ebox41 = tk.Entry(width=2,borderwidth=1)
+ebox41.insert(tk.END,4)
+ebox41.place(x=335,y=135)
+
+label2_42 = tk.Label(root,text='42歳          号昇給')
+label2_42.place(x=295,y=155)
+ebox42 = tk.Entry(width=2,borderwidth=1)
+ebox42.insert(tk.END,4)
+ebox42.place(x=335,y=155)
+
+label2_43 = tk.Label(root,text='43歳          号昇給')
+label2_43.place(x=295,y=175)
+ebox43 = tk.Entry(width=2,borderwidth=1)
+ebox43.insert(tk.END,4)
+ebox43.place(x=335,y=175)
+
+label2_44 = tk.Label(root,text='44歳          号昇給')
+label2_44.place(x=295,y=195)
+ebox44 = tk.Entry(width=2,borderwidth=1)
+ebox44.insert(tk.END,4)
+ebox44.place(x=335,y=195)
+
+label2_45 = tk.Label(root,text='45歳          号昇給')
+label2_45.place(x=295,y=215)
+ebox45 = tk.Entry(width=2,borderwidth=1)
+ebox45.insert(tk.END,4)
+ebox45.place(x=335,y=215)
+
+label2_46 = tk.Label(root,text='46歳          号昇給')
+label2_46.place(x=295,y=235)
+ebox46 = tk.Entry(width=2,borderwidth=1)
+ebox46.insert(tk.END,4)
+ebox46.place(x=335,y=235)
+
+label2_47 = tk.Label(root,text='47歳          号昇給')
+label2_47.place(x=295,y=255)
+ebox47 = tk.Entry(width=2,borderwidth=1)
+ebox47.insert(tk.END,4)
+ebox47.place(x=335,y=255)
+
+label2_48 = tk.Label(root,text='48歳          号昇給')
+label2_48.place(x=295,y=275)
+ebox48 = tk.Entry(width=2,borderwidth=1)
+ebox48.insert(tk.END,4)
+ebox48.place(x=335,y=275)
+
+label2_49 = tk.Label(root,text='49歳          号昇給')
+label2_49.place(x=295,y=295)
+ebox49 = tk.Entry(width=2,borderwidth=1)
+ebox49.insert(tk.END,4)
+ebox49.place(x=335,y=295)
+
+label2_50 = tk.Label(root,text='50歳          号昇給')
+label2_50.place(x=295,y=315)
+ebox50 = tk.Entry(width=2,borderwidth=1)
+ebox50.insert(tk.END,4)
+ebox50.place(x=335,y=315)
+
+label2_51 = tk.Label(root,text='51歳          号昇給')
+label2_51.place(x=435,y=135)
+ebox51 = tk.Entry(width=2,borderwidth=1)
+ebox51.insert(tk.END,4)
+ebox51.place(x=475,y=135)
+
+label2_52 = tk.Label(root,text='52歳          号昇給')
+label2_52.place(x=435,y=155)
+ebox52 = tk.Entry(width=2,borderwidth=1)
+ebox52.insert(tk.END,4)
+ebox52.place(x=475,y=155)
+
+label2_53 = tk.Label(root,text='53歳          号昇給')
+label2_53.place(x=435,y=175)
+ebox53 = tk.Entry(width=2,borderwidth=1)
+ebox53.insert(tk.END,4)
+ebox53.place(x=475,y=175)
+
+label2_54 = tk.Label(root,text='54歳          号昇給')
+label2_54.place(x=435,y=195)
+ebox54 = tk.Entry(width=2,borderwidth=1)
+ebox54.insert(tk.END,4)
+ebox54.place(x=475,y=195)
+
+label2_55 = tk.Label(root,text='55歳          号昇給')
+label2_55.place(x=435,y=215)
+ebox55 = tk.Entry(width=2,borderwidth=1)
+ebox55.insert(tk.END,4)
+ebox55.place(x=475,y=215)
+
+label2_56 = tk.Label(root,text='56歳          号昇給')
+label2_56.place(x=435,y=235)
+ebox56 = tk.Entry(width=2,borderwidth=1)
+ebox56.insert(tk.END,0)
+ebox56.place(x=475,y=235)
+
+label2_57 = tk.Label(root,text='57歳          号昇給')
+label2_57.place(x=435,y=255)
+ebox57 = tk.Entry(width=2,borderwidth=1)
+ebox57.insert(tk.END,0)
+ebox57.place(x=475,y=255)
+
+label2_58 = tk.Label(root,text='58歳          号昇給')
+label2_58.place(x=435,y=275)
+ebox58 = tk.Entry(width=2,borderwidth=1)
+ebox58.insert(tk.END,0)
+ebox58.place(x=475,y=275)
+
+label2_59 = tk.Label(root,text='59歳          号昇給')
+label2_59.place(x=435,y=295)
+ebox59 = tk.Entry(width=2,borderwidth=1)
+ebox59.insert(tk.END,0)
+ebox59.place(x=475,y=295)
+
+label2_60 = tk.Label(root,text='60歳          号昇給')
+label2_60.place(x=435,y=315)
+ebox60 = tk.Entry(width=2,borderwidth=1)
+ebox60.insert(tk.END,0)
+ebox60.place(x=475,y=315)
+
+
+
+label3_1 = tk.Label(root,text='(3)-1 主任に昇任する年齢を入力してください')
+label3_1.place(x=5,y=335)
+editbox3_1 = tk.Entry(width=2,borderwidth=1)
+editbox3_1.place(x=15,y=355)
+
+label3_2 = tk.Label(root,text='(3)-2 係長に昇任する年齢を入力してください')
+label3_2.place(x=5,y=375)
+editbox3_2 = tk.Entry(width=2,borderwidth=1)
+editbox3_2.place(x=15,y=395)
+
+label3_3 = tk.Label(root,text='(3)-3 課長に昇任する年齢を入力してください')
+label3_3.place(x=5,y=415)
+editbox3_3 = tk.Entry(width=2,borderwidth=1)
+editbox3_3.place(x=15,y=435)
+
+label3_4 = tk.Label(root,text='(3)-4 部長に昇任する年齢を入力してください')
+label3_4.place(x=5,y=455)
+editbox3_4 = tk.Entry(width=2,borderwidth=1)
+editbox3_4.place(x=15,y=475)
+
+label4 = tk.Label(root,text='(4) 入都時の初任給（号給）を入力して下さい（通常２９号給）。')
+label4.place(x=5,y=495)
+editbox4 = tk.Entry(width=2,borderwidth=1)
+editbox4.place(x=15,y=515)
+
+
+
+def program():
+    '''
+    GUIに入力された値に基づいて、シミュレーションプログラムを実行する。
+    '''
+
+    import main_program_GUI as mp
+    import pandas as pd
+    import numpy as np 
+    from tkinter import messagebox
+
+    # 入都年齢
+    if (int(editbox1.get())<23) or (int(editbox1.get())>60) :
+        editbox1.delete(0,tk.END)
+        editbox1.insert(tk.END,23)
+    val1 = int(editbox1.get())
+
+    # 入都してから退職までの一年毎の昇給幅
+    class out_of_range_exception(Exception):
+        pass
+    list_of_ebox2 = [ebox23,ebox24,ebox25,ebox26,ebox27,ebox28,ebox29,ebox30,
+                     ebox31,ebox32,ebox33,ebox34,ebox35,ebox36,ebox37,ebox38,ebox39,ebox40,
+                     ebox41,ebox42,ebox43,ebox44,ebox45,ebox46,ebox47,ebox48,ebox49,ebox50,
+                     ebox51,ebox52,ebox53,ebox54,ebox55,ebox56,ebox57,ebox58,ebox59,ebox60]
+    assessments = [assessment.get() for assessment in list_of_ebox2]
+    for i in assessments:
+        if (int(i) < 0 ) or (6 < int(i)):
+            messagebox.showwarning('入力エラー','昇給幅は０から６までの数字を入力して下さい。')
+            raise out_of_range_exception(i)
+    val2 = pd.DataFrame({'assessment':assessments},index=range(23,61)) 
+    
+
+    list = [editbox3_1.get(),editbox3_2.get(),editbox3_3.get(),editbox3_4.get()]
+    list_of_val3=[]
+    over_60 = 61
+    for j in list:
+        if j == '':
+            j = over_60
+            over_60 = over_60 + 1   
+        list_of_val3.append(int(j))
+    val3_1=list_of_val3[0]
+    val3_2=list_of_val3[1]
+    val3_3=list_of_val3[2]
+    val3_4=list_of_val3[3]
+
+
+    '''
+    val3_1=int(editbox3_1.get())
+    val3_2=int(editbox3_2.get())
+    val3_3=int(editbox3_3.get())
+    val3_4=int(editbox3_4.get())
+    '''
+    val3 = pd.Series(range(1,6),index=[val1,val3_1,val3_2,val3_3,val3_4])   
+    val4 = int(editbox4.get())
+
+    df_all = mp.simulate(val1,val2,val3,val4)
+    print(df_all)
+    how_much_i_earn = str(df_all.iloc[37,8])[:-3] 
+    print(how_much_i_earn)
+    
+    editbox5 = tk.Entry(width=30,borderwidth=1)
+    editbox5.insert(tk.END,how_much_i_earn + '円       ')
+    editbox5.place(x=5,y=595)
+
+
+button = tk.Button(root,text='上の条件で生涯年収を計算する(PUSH ME!)',command = program,width=59,borderwidth=3)
+button.place(x=5,y=535)
+
+label5 = tk.Label(root,text='上のボタンを押すと、ここにシミュレーション結果が表示されます。')
+label5.place(x=5,y=575)
+
+
+
+
+root.mainloop()
---
+++ main_program_GUI.py
@@ -0,0 +1,145 @@
+import numpy as np
+import pandas as pd
+
+# 毎年の昇給
+assessment_low =pd.DataFrame({'assessment':[    0,4,4,4,4,4,4,4,
+                                            4,4,4,4,4,4,4,4,4,4,
+                                            4,4,4,4,4,4,4,4,4,4,
+                                            4,4,4,4,4,0,0,0,0,0]},
+                              index = range(23,61))
+assessment_middle=pd.DataFrame({'assessment':[    0,4,4,6,5,5,4,4,
+                                              4,4,4,4,4,6,5,5,4,4,
+                                              4,4,4,4,4,6,5,5,4,4,
+                                              4,4,4,4,4,2,1,1,0,0]},
+                               index = range(23,61))
+assessment_high  =pd.DataFrame({'assessment':[    0,5,5,6,5,5,5,5,
+                                              5,5,5,5,5,6,5,5,5,5,
+                                              5,5,5,5,5,6,5,5,5,5,
+                                              5,5,5,5,5,2,1,1,1,1]},
+                                index = range(23,61))
+assessment_higer =pd.DataFrame({'assessment':[    0,6,6,6,6,6,6,6,
+                                              6,6,6,6,6,6,6,6,6,6,
+                                              6,6,6,6,6,6,6,6,6,6,
+                                              6,6,6,6,6,2,2,2,2,2]},
+                                index = range(23,61))
+my_assessment_low  =pd.DataFrame({'assessment':[    0,0,0,0,4,4,4,4,
+                                                5,4,4,4,4,4,4,4,4,5,
+                                                5,4,4,4,4,4,4,4,4,5,
+                                                5,4,4,4,4,0,0,0,0,0]},
+                                index = range(23,61))
+my_assessment_middle=pd.DataFrame({'assessment':[    0,0,0,0,4,4,4,4,
+                                                 5,4,4,4,4,4,4,4,6,5,
+                                                 5,4,4,4,4,4,4,4,6,5,
+                                                 5,4,4,4,4,0,0,2,1,1]},
+                                   index = range(23,61))
+
+# 昇格するパターン
+shoukaku_slow_pattern = pd.Series(range(1,6),index=[23,31,37,43,50])
+shoukaku_middle_pattern = pd.Series(range(1,6),index=[23,29,35,40,45])
+shoukaku_fast_pattern = pd.Series(range(1,6),index=[23,28,33,37,42])
+shoukaku_my_pattern = pd.Series(range(1,6),index=[26,33,41,61,62])
+
+def goukyuu(age = 26,
+            shoukyuu_pattern = my_assessment_low,
+            shoukaku_pattern = shoukaku_my_pattern,
+            starting_salary = 29):
+    '''
+    age:入都した時の年齢
+    shoukyuu_pattern:昇級パターン,pd.DataFrame
+    shoukaku_pattern:昇格する年齢のパターン,pd.Series
+    starting_salary:初任給
+    役割　　入都から退職までの自身の毎年の級、号給を決める。
+    引数　　age:入都時の号給　pattern:業績評価のパターンassessment_〇〇と言う変数の、データフレーム
+    戻り値　pandasのDataFrame 
+    '''
+
+    # インデックスが年齢、１列が職級、２列が号給
+    df_shoukakushoukyuu = pd.read_csv('g1_shoukaku.csv')
+    df_getugakuh = pd.read_csv('g1_kyuuryou.csv') 
+   
+    shokukyuu_each_year = shoukaku_pattern.reindex(range(23,61),method='ffill')
+    salary_table = pd.DataFrame(shokukyuu_each_year)
+    salary_table['Step']=shoukyuu_pattern
+    salary_table.iloc[age-23,1]=starting_salary
+    salary_table= salary_table.rename(columns={0:'Grade'})
+    
+    for i in range(age+1,61):
+        if int(salary_table.iloc[i-23,0])==5:
+            salary_table.iloc[i-23,1]=2 #部長のランクは２給で固定となっている。この部分は改良の余地がある。
+        elif salary_table.iloc[i-23,0]==salary_table.iloc[i-24,0]:
+            salary_table.iloc[i-23,1]=int(salary_table.iloc[i-24,1])+int(salary_table.iloc[i-23,1])     
+        elif (salary_table.iloc[i-23,0]>salary_table.iloc[i-24,0]) and (5>salary_table.iloc[i-23,0]):
+            number = int(salary_table.iloc[i-23,0]-1)
+            salary_table.iloc[i-23,1]=int(salary_table.iloc[i-24,1])+int(salary_table.iloc[i-23,1])#昇格した時の昇給
+            salary_table.iloc[i-23,1]=int(df_shoukakushoukyuu.iloc[int(salary_table.iloc[i-23,1]-1),number]) 
+    return salary_table
+         
+def monthly_salary(salary_table):
+    '''
+    役割    毎年の給料月額（/月）と地域手当（/月）を求める。
+    引数    salary_table : goukyuu()の戻り値。DataFrameオブジェクト。
+    戻り値  df_monthly_salary : 毎年の給料月額（/月）と地域手当（/月）の支給額が分かるデータフレーム
+    ''' 
+
+    df_monthly_salary = pd.DataFrame(index=range(23,61))
+    df_monthly_salary['monthly_salary']=0
+    df_monthly_salary['area_allowance']=0
+    df_g1_kyuuryou = pd.read_csv('g1_kyuuryou.csv')
+    for i in range(23,61):
+        for j in range(1,6):
+            if salary_table.iloc[i-23,0]==j:
+                df_monthly_salary.iloc[i-23,0]=df_g1_kyuuryou.iloc[salary_table.iloc[i-23,1]-1,j]
+                df_monthly_salary.iloc[i-23,1]=df_monthly_salary.iloc[i-23,0]*0.2
+    return df_monthly_salary
+
+def management_allowance(salary_table):
+    df_management_allowance = pd.DataFrame(index=range(23,61))
+    df_management_allowance['management_allowance']=0
+    for i in range(23,61):
+        if salary_table.iloc[i-23,0]==4:
+            df_management_allowance.iloc[i-23,0]=80000
+        elif salary_table.iloc[i-23,0]==5:
+            df_management_allowance.iloc[i-23,0]=126900
+    return df_management_allowance
+
+def bounus(salary_table,df_monthly_salary):
+    '''
+    成績率、管理職加算、職務段階別加算によって賞与支給額を変化させるコードは、現時点では書かない。
+    書くときに、salary_tableを使用する予定。
+    役割　毎年の特別給支給額を求める。
+    引数　salary_table : goukyuu()の戻り値。データフレーム。
+          df_monthly_salary() ; monthly_salary()の戻り値。
+    戻り値　df_bounus : 毎年の特別給支給額が分かるデータフレーム。    
+    '''
+    
+    df_bounus = pd.DataFrame(index=range(23,61))
+    df_bounus['bounus']=0
+    for i in range(23,61):
+        kisogaku_large = df_monthly_salary.iloc[i-23,0]+df_monthly_salary.iloc[i-23,1]
+        kisogaku_small = df_monthly_salary.iloc[i-23]
+        df_bounus.iloc[i-23,0]=kisogaku_large*4.5
+
+    return df_bounus
+
+
+def simulate(age = 26,
+             shoukyuu_pattern = my_assessment_low,
+             shoukaku_pattern = shoukaku_my_pattern,
+             starting_salary = 29):
+    salary_table=goukyuu(age,shoukyuu_pattern,shoukaku_pattern,starting_salary)
+    df_monthly_salary = monthly_salary(salary_table)
+    df_management_allowance = management_allowance(salary_table)
+    df_bounus = bounus(salary_table,df_monthly_salary)
+
+    df_all = pd.concat([salary_table,df_monthly_salary,df_management_allowance,df_bounus],axis=1)
+
+    df_all['monthly_income']=df_all.sum(axis=1)-df_all['bounus']
+    df_all['annual_income']=df_all['monthly_income']*12+df_all['bounus']
+
+    df_all['lifetime_annual_income'] = df_all['annual_income'].cumsum()
+    return(df_all)
+
+if __name__ == '__main__':
+    df_all = simulate()
+    print(df_all)
+
